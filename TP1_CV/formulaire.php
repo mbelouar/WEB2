1ddd2a90074be4e43b4a7230165e126e9cbe52fa
mbelouar <simobelouarraq5@gmail.com>
2025-02-15 23:15:05 +0100

    make the fields required

--- TP1_CV/formulaire.php
+++ TP1_CV/formulaire.php
@@ -24,24 +24,24 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <label for="firstname" class="form-label">Prénom <span class="blue">*</span></label>
-                            <input id="firstname" type="text" name="firstname" class="form-control" placeholder="Votre prénom">
+                            <input id="firstname" type="text" name="firstname" class="form-control" placeholder="Votre prénom" required>
                         </div>
                         <div class="col-lg-6">
                             <label for="name" class="form-label">Nom <span class="blue">*</span></label>
-                            <input id="name" type="text" name="name" class="form-control" placeholder="Votre Nom">
+                            <input id="name" type="text" name="name" class="form-control" placeholder="Votre Nom" required>
                         </div>
                         <div class="col-lg-6">
                             <label for="email" class="form-label">Email <span class="blue">*</span></label>
-                            <input id="email" type="text" name="email" class="form-control" placeholder="Votre Email">
+                            <input id="email" type="text" name="email" class="form-control" placeholder="Votre Email" required>
                         </div>
                         <div class="col-lg-6">
-                            <label for="phone" class="form-label">Téléphone</label>
-                            <input id="phone" type="text" name="phone" class="form-control" placeholder="Votre Téléphone">
+                            <label for="phone" class="form-label">Téléphone <span class="blue">*</span></label>
+                            <input id="phone" type="text" name="phone" class="form-control" placeholder="Votre Téléphone" required>
                         </div>
                         <div class="row">
                         <div class="col-lg-6">
                             <label for="age" class="form-label">Age <span class="blue">*</span></label>
-                            <input id="age" type="text" name="age" class="form-control" placeholder="Votre Age">
+                            <input id="age" type="text" name="age" class="form-control" placeholder="Votre Age" required>
                         </div>
                     </div>
                     </div>
@@ -54,74 +54,74 @@
                         <div class="col-lg-12">
                             <label for="formation" class="form-label">Vous êtes en: <span class="blue">*</span></label><br>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="2AP" id="formation-2AP">
+                                <input class="form-check-input" type="radio" name="formation" value="2AP" id="formation-2AP" required>
                                 <label class="form-check-label" for="formation-2AP">2AP</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="GSTR" id="formation-GSTR">
+                                <input class="form-check-input" type="radio" name="formation" value="GSTR" id="formation-GSTR" required>
                                 <label class="form-check-label" for="formation-GSTR">GSTR</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="GI" id="formation-GI">
+                                <input class="form-check-input" type="radio" name="formation" value="GI" id="formation-GI" required>
                                 <label class="form-check-label" for="formation-GI">GI</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="SCM" id="formation-SCM">
+                                <input class="form-check-input" type="radio" name="formation" value="SCM" id="formation-SCM" required>
                                 <label class="form-check-label" for="formation-SCM">SCM</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="GC" id="formation-GC">
+                                <input class="form-check-input" type="radio" name="formation" value="GC" id="formation-GC" required>
                                 <label class="form-check-label" for="formation-GC">GC</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="formation" value="MS" id="formation-MS">
+                                <input class="form-check-input" type="radio" name="formation" value="MS" id="formation-MS" required>
                                 <label class="form-check-label" for="formation-MS">MS</label>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="niveau" value="niveau_1" id="niveau_1">
+                                <input class="form-check-input" type="radio" name="niveau" value="niveau_1" id="niveau_1" required>
                                 <label class="form-check-label" for="niveau_1">1er annee</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="niveau" value="niveau_2" id="niveau_2">
+                                <input class="form-check-input" type="radio" name="niveau" value="niveau_2" id="niveau_2" required>
                                 <label class="form-check-label" for="niveau_2">2eme annee</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="niveau" value="niveau_3" id="niveau_3">
+                                <input class="form-check-input" type="radio" name="niveau" value="niveau_3" id="niveau_3" required>
                                 <label class="form-check-label" for="niveau_3">3eme annee</label>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <label for="formation" class="form-label">Modules suivies cette annee: <span class="blue">*</span></label><br>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="proAv" id="proAv">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="proAv" id="proAv" required>
                                 <label class="form-check-label" for="proAv">Pro Av</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="compilation" id="compilation">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="compilation" id="compilation" required>
                                 <label class="form-check-label" for="compilation">Compilation</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="reseauxAv" id="reseauxAv">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="reseauxAv" id="reseauxAv" required>
                                 <label class="form-check-label" for="reseauxAv">Reseaux Av</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="webAv" id="webAv">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="webAv" id="webAv" required>
                                 <label class="form-check-label" for="webAv">Web Avancee</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="poo" id="poo">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="poo" id="poo" required>
                                 <label class="form-check-label" for="poo">POO</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="modules[]" value="bd" id="bd">
+                                <input class="form-check-input" type="checkbox" name="modules[]" value="bd" id="bd" required>
                                 <label class="form-check-label" for="bd">BD</label>
                             </div>
                         </div>
                         <div class="col-lg-12">
                             <label for="project" class="form-label">Nombre de projets réalisés cette année: <span class="blue">*</span></label>
-                            <select id="project" name="project" class="form-control" onchange="generateProjectFields()">
+                            <select id="project" name="project" class="form-control" onchange="generateProjectFields()" required>
                                 <option value="0">0</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
@@ -142,11 +142,11 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <label for="interest1" class="form-label">Centre d'intérêt 1: <span class="blue">*</span></label>
-                            <input id="interest1" type="text" name="interest1" class="form-control" placeholder="Entrez votre intérêt">
+                            <input id="interest1" type="text" name="interest1" class="form-control" placeholder="Entrez votre intérêt" required>
                         </div>
                         <div class="col-lg-6">
                             <label for="interest2" class="form-label">Centre d'intérêt 2: <span class="blue">*</span></label>
-                            <input id="interest2" type="text" name="interest2" class="form-control" placeholder="Entrez votre intérêt">
+                            <input id="interest2" type="text" name="interest2" class="form-control" placeholder="Entrez votre intérêt" required>
                         </div>
                         <div class="col-lg-6 mt-2">
                             <label for="interest3" class="form-label">Centre d'intérêt 3:</label>
@@ -165,11 +165,11 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <label for="langue1" class="form-label">Langue 1: <span class="blue">*</span></label>
-                            <input id="langue1" type="text" name="langue1" class="form-control" placeholder="Entrez la langue">
+                            <input id="langue1" type="text" name="langue1" class="form-control" placeholder="Entrez la langue" required>
                         </div>
                         <div class="col-lg-6">
                             <label for="niveau1" class="form-label">Niveau: <span class="blue">*</span></label>
-                            <select id="niveau1" name="niveau1" class="form-control">
+                            <select id="niveau1" name="niveau1" class="form-control" required>
                                 <option value="debutant">Débutant</option>
                                 <option value="intermediaire">Intermédiaire</option>
                                 <option value="avance">Avancé</option>
@@ -177,11 +177,11 @@
                         </div>
                         <div class="col-lg-6 mt-2">
                             <label for="langue2" class="form-label">Langue 2: <span class="blue">*</span></label>
-                            <input id="langue2" type="text" name="langue2" class="form-control" placeholder="Entrez la langue">
+                            <input id="langue2" type="text" name="langue2" class="form-control" placeholder="Entrez la langue" required>
                         </div>
                         <div class="col-lg-6 mt-2">
                             <label for="niveau2" class="form-label">Niveau: <span class="blue">*</span></label>
-                            <select id="niveau2" name="niveau2" class="form-control">
+                            <select id="niveau2" name="niveau2" class="form-control" required>
                                 <option value="debutant">Débutant</option>
                                 <option value="intermediaire">Intermédiaire</option>
                                 <option value="avance">Avancé</option>
@@ -203,7 +203,6 @@
                 </div>
 
 
-                <!-- Remarques -->
                 <!-- Remarques -->
                 <div class="section">
                     <div class="section-title">Vos Remarques</div>
